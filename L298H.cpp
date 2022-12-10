37b8e581e51303d4748e1bf697dff6b313c64f9f
David73-Coloa <xdsomu851@gmail.com>
2022-12-10 06:45:24 -0300

    Just some changes, after a year

--- L298H.cpp
+++ L298H.cpp
@@ -5,41 +5,33 @@
 
 #include <L298H.h>
 
-  L298H::L298H() {  
-    this -> speed = 20;
-  }
-    
-    void  L298H::setSpeed(int Speed){ 
-      speed = Speed;
-    }
-
-    void  L298H::Pinout(int in1,int in2,int en) {
-      this -> pin1 = in1;
-      this -> pin2 = in2;
-      this -> enable = en;
-      pinMode(pin1, OUTPUT);
-      pinMode(pin2, OUTPUT);
-      pinMode(enable,OUTPUT);
-    }
-
-    
-    void  L298H::goForward(){ 
-      digitalWrite(pin1, HIGH);
-      digitalWrite(pin2, LOW);
-      analogWrite(enable,speed);
-      
-    }   
-
-    void  L298H::goBackward(){ 
-      digitalWrite(pin1, LOW);
-      digitalWrite(pin2, HIGH);
-      analogWrite(enable,speed);
-      
-    }
-
-    void L298H::stop() {
-      digitalWrite(pin1,LOW);
-      digitalWrite(pin2,LOW);
-      analogWrite(enable,0);
-    }
+L298H::L298H() {  
+  this -> speed = 20;
+}
+void  L298H::setSpeed(int Speed){ 
+  speed = Speed;
+}
+void  L298H::Pinout(int in1,int in2,int en) {
+  this -> pin1 = in1;
+  this -> pin2 = in2;
+  this -> enable = en;
+  pinMode(pin1, OUTPUT);
+  pinMode(pin2, OUTPUT);
+  pinMode(enable,OUTPUT);
+}
+void  L298H::goForward(){ 
+  digitalWrite(pin1, HIGH);
+  digitalWrite(pin2, LOW);
+  analogWrite(enable,speed);
+}   
+void  L298H::goBackward(){ 
+  digitalWrite(pin1, LOW);
+  digitalWrite(pin2, HIGH);
+  analogWrite(enable,speed);
+}
+void L298H::stop() {
+  digitalWrite(pin1,LOW);
+  digitalWrite(pin2,LOW);
+  analogWrite(enable,0);
+}
  
